6756735648a5a3cc64387bfec5fc95b2a213c0a5
spencerwilson123321 <spencerwilson123321@gmail.com>
2022-09-15 11:10:00 -0700

    added watch constant and removed menu string const

--- constants.py
+++ constants.py
@@ -2,7 +2,7 @@
 START_KEYLOGGER = "start keylogger"
 STOP_KEYLOGGER = "stop keylogger"
 TRANSFER_KEYLOGGER = "transfer keylogger"
+WATCH = "watch"
 QUIT = "quit"
 CLEAR = "clear"
 HELP = "help"
-MENU_STRING = "Welcome to the rootkit!\nHere are your options:\n1. Start the keylogger\n2. Stop the keylogger\n3. Transfer keylogger\n4. Disconnect/Terminate\n"
