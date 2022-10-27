bedeba3c0dc122179cd0933f465e1cbecc5ec25f
spencerwilson123321 <spencerwilson123321@gmail.com>
2022-10-27 10:56:54 -0700

    Updated shell text

--- utils/shell.py
+++ utils/shell.py
@@ -20,7 +20,7 @@ def clear_screen():
 
 def print_menu():
     clear_screen()
-    print("Welcome to the Backdoor Command Shell!")
+    print("Welcome to the Rootkit Command Shell!")
     print("Enter 'help' for a list of commands")
 
 def print_help():
@@ -30,4 +30,5 @@ def print_help():
     print("exit " + 4*"\t" + " End the terminal session.")
     print("keylogger [stop | start | get]" + 1*"\t" + " Stop, start, or get keylogger on rootkit.")
     print("watch [dirpath | filepath]" + 1*"\t" + " Watch the given directory or file for changes.")
-    
+    print("list dirpath" + 3*"\t" + " Attempts to list the contents of the given 'dirpath'.")
+    print("wget url dirpath" + 2*"\t" + " Downloads the resource from 'url' to the given 'dirpath'.")
