9f9f1faa9c24247c26a504f4fbbf622af213216c
spencerwilson123321 <spencerwilson123321@gmail.com>
2022-11-04 10:31:44 -0700

    removed comments

--- utils/process.py
+++ utils/process.py
@@ -18,13 +18,3 @@ def hide_process_name(name: str) -> None:
         None
     """
     setproctitle(name)
-    # Get the PID of the process.
-    # cdll.LoadLibrary("libc.so.6")
-    # libc = CDLL("libc.so.6")
-    # pr_set_name = c_ulong(15)
-    # zero = c_ulong(0)
-    # newname = bytes("newname", encoding="ascii")
-    # libc.prctl(pr_set_name, name, zero, zero, zero)
-    # libc.strcpy(sys.argv[0], newname.value)
-    # print(sys.argv[0])
-    # print(newname.value)
